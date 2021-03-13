f3e2f45a17eeaf7eef3017b1a99ce702f11b2841
Ayshik Halder <halderayshik@gmail.com>
2021-03-13 23:35:12 +0530

    13 MAR projects add

--- js/scripts.js
+++ js/scripts.js
@@ -163,7 +163,7 @@
       	$('.pagepiling').pagepiling({
     		scrollingSpeed: 280,
 		    loopBottom:true,
-		    anchors: ['Home', 'Specialization', 'Education', 'Skills', 'Projects', 'tools', 'Testimonials', 'Contacts'],
+		    anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
 		    afterLoad: function(anchorLink, index){
 	           navbarFullpage();
 	            
